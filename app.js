bfced446dbb21288debe25eda0a102fe25bf921d
TubiOb <Tubiobaloluwa@gmail.com>
2023-11-26 12:50:50 +0100

    Working on the accordion

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ function changeBorderColor(isFocused) {
     var searchBox = document.querySelector('.search-box');
     searchBox.style.borderColor = isFocused ? '4px solid #F0F0F0' : '1px solid #616161';
 }
+
+function cancelTrialCallout() {
+    // Assuming trialCallout is the parent container
+    var trialCallout = document.querySelector('.trialCallout');
+    trialCallout.style.display = 'none'; // Hide the trialCallout
+}
